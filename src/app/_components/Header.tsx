0520430d351c988703715bb84b4fc3a199d3d62e
Guilherme Freitas <guilhermeaqf@gmail.com>
2025-08-03 02:58:18 -0300

    feat: add sidebar

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,19 +1,92 @@
-import { Card, CardContent } from "@/app/_components/ui/card";
-import  Image  from 'next/image';
-import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import { Card, CardContent } from "@/app/_components/ui/card"
+import Image from "next/image"
+import { Button } from "./ui/button"
+import {
+  CalendarIcon,
+  HomeIcon,
+  LogOutIcon,
+  MenuIcon,
+  UserIcon,
+} from "lucide-react"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet"
+import { quickSearchOptions } from "../_constants/search"
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import Link from "next/link"
 
 const Header = () => {
-    return ( 
-        <Card>
-            <CardContent className="flex flex-row items-center justify-between p-5">
-                <Image src="/logo.png" alt={"Logo"} height={18} width={70}/>
-                <Button size="icon" variant="outline">
-                    <MenuIcon/>
+  return (
+    <Card>
+      <CardContent className="flex flex-row items-center justify-between p-5">
+        <Image src="/logo.png" alt={"Logo"} height={18} width={70} />
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle className="text-left">Menu</SheetTitle>
+            </SheetHeader>
+            <div className="flex items-center gap-3 border-b border-solid py-5">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col">
+                <p className="text-sm font-bold">teste</p>
+                <p className="text-xs">teste@teste.com</p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 border-b border-solid py-5">
+              <SheetClose asChild>
+                <Button className="justify-start gap-2" variant="ghost" asChild>
+                  <Link href="/">
+                    <HomeIcon size={18} />
+                    Início
+                  </Link>
                 </Button>
-            </CardContent>
-        </Card>
-     );
+              </SheetClose>
+              <Button variant="ghost" className="justify-start gap-2">
+                <CalendarIcon size={18} />
+                Agendamentos
+              </Button>
+            </div>
+            <div className="flex flex-col gap-2 border-b border-solid py-5">
+              {quickSearchOptions.map((option) => (
+                <Button
+                  key={option.name}
+                  variant="ghost"
+                  className="justify-start gap-2"
+                >
+                  <Image
+                    src={option.imageUrl}
+                    alt={option.name}
+                    height={18}
+                    width={18}
+                  />
+                  {option.name}
+                </Button>
+              ))}
+            </div>
+            <div className="flex flex-col gap-2 py-5">
+              <Button variant="ghost" className="justify-start">
+                <LogOutIcon size={18} />
+                Sair da conta
+              </Button>
+            </div>
+          </SheetContent>
+        </Sheet>
+      </CardContent>
+    </Card>
+  )
 }
- 
-export default Header;
+
+export default Header
---
+++ src/app/_components/service-item.tsx
@@ -0,0 +1,5 @@
+const ServiceItem = () => {
+    return ( <></> );
+}
+ 
+export default ServiceItem;
